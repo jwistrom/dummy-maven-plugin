dbfb845a06c99fa66adca2a935feee93e04d4263
Johan Wiström <johan.wistrom@reforceinternational.com>
2019-05-11 18:55:05 +0200

    done

--- src/main/java/com/wistrom/plugin/dummy/PrintJohanMojo.java
+++ src/main/java/com/wistrom/plugin/dummy/PrintJohanMojo.java
@@ -1,4 +1,14 @@
 package com.wistrom.plugin.dummy;
 
-public class PrintJohanMojo {
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "printJohan")
+public class PrintJohanMojo extends AbstractMojo {
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("---------I AM NOW PRINTING JOHAN---------");
+    }
 }
