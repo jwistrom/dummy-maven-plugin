d2d27e78ae3e7d4de3de4afd0c4fa38b5fae57bd
Johan Wistrom <johan.wistrom@pricerunner.com>
2019-06-10 11:13:05 +0200

    color printing

--- src/main/java/com/wistrom/plugin/dummy/PrintJohanMojo.java
+++ src/main/java/com/wistrom/plugin/dummy/PrintJohanMojo.java
@@ -8,7 +8,11 @@ import org.apache.maven.plugins.annotations.Mojo;
 @Mojo(name = "printJohan")
 public class PrintJohanMojo extends AbstractMojo {
 
+    private static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_WHITE = "\u001B[37m";
+
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("---------I AM NOW PRINTING JOHAN---------");
+        getLog().info(ANSI_RED_BACKGROUND + ANSI_WHITE + "---------I AM NOW PRINTING JOHAN---------" + ANSI_RESET);
     }
 }
